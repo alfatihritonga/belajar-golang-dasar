a95d3980232b9ac5cba5628b022c2cd0cf2fcc02
Muhammad Al-fatih Ritonga <muhammadalfatihritonga22@gmail.com>
2025-01-03 16:35:55 +0700

    tipe data string

---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad A-fatih Ritonga")
+	fmt.Println(len("Muhammad A-fatih Ritonga"))
+	fmt.Println("Muhammad Al-fatih Ritonga"[0])
+	fmt.Println("Muhammad Al-fatih Ritonga"[10])
+}
+
+// Note
+
+// Fucntion untuk String
+// - len("string") => menghitung panjang karakter *karakter "whitespace juga dihitung"
+// - "string"[index] => mendapat karakter berdasarkan posisi indexnya
