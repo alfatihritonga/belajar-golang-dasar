52ce86aab57719137d0ab6bb630c9fb1273b0150
Muhammad Al-fatih Ritonga <muhammadalfatihritonga22@gmail.com>
2025-01-07 19:07:01 +0700

    access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,20 @@
 package helper
 
+import "fmt"
+
+var version = "1.0.0" // hanya bisa diakses didalam package yg sama
+var Application = "Golang"
+
+// function dibawah tidak dapat diakses diluar package helper
+func sayGoodBye(v string) string {
+	return "Good Bye " + v
+}
+
 func SayHello(v string) string {
 	return "Hello " + v
+}
+
+func Contoh() {
+	fmt.Println(sayGoodBye("Fatih"))
+	fmt.Println(version)
 }
--- import.go
+++ import.go
@@ -8,4 +8,9 @@ import (
 func main() {
 	res := helper.SayHello("Fatih")
 	fmt.Println(res)
+
+	// fmt.Println(helper.version) // tidak dapat diakses
+	fmt.Println(helper.Application) // bisa diakses diluar packagenya
+	// fmt.Println(helper.sayGoodBye("Fatih")) // tidak dapat diakses
+	helper.Contoh()
 }
