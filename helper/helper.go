af7b1d7a09bb3a2cd18a0a44a498a1a9ba2bf8a2
Muhammad Al-fatih Ritonga <muhammadalfatihritonga22@gmail.com>
2025-01-07 18:58:05 +0700

    package & import: mengelola package dan menggunakannya

---
+++ helper/helper.go
@@ -0,0 +1,5 @@
+package helper
+
+func SayHello(v string) string {
+	return "Hello " + v
+}
---
+++ import.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"belajar-golang-dasar/helper"
+	"fmt"
+)
+
+func main() {
+	res := helper.SayHello("Fatih")
+	fmt.Println(res)
+}
